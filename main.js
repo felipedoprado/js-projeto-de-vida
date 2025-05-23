6f303afdd5c9b952a597cc1cbd9e06150e761dcf
felipedoprado <145500571+felipedoprado@users.noreply.github.com>
2025-05-23 18:59:22 +0000

    botao

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
+
 for(let i=0; i < botoes.length; i++){
     botoes[i].onclick = function(){
-        for(let j = 0; j < botoes; j++){
+        for(let j=0; j<botoes.length; j++){
             botoes[j].classList.remove("ativo");
         }
+        botoes[i].classList.add("ativo");
     }
 }
