f7f5db0ad208558c669cf98d5306977ad4e17593
felipedoprado <145500571+felipedoprado@users.noreply.github.com>
2025-05-30 17:50:46 +0000

    fix

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
-const texto = document.querySelectorAll(".aba-conteudo")
+const textos = document.querySelectorAll(".aba-conteudo");
+
 for(let i=0; i < botoes.length; i++){
     botoes[i].onclick = function(){
         for(let j=0; j<botoes.length; j++){
@@ -11,7 +12,7 @@ for(let i=0; i < botoes.length; i++){
     }
 }
 
-const contadores = document.querySelectorAll(".contador")
+const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
 
 contadores[0].textContent = ;
@@ -19,14 +20,14 @@ contadores[0].textContent = ;
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
-    let segundos = Math.floor(tempoFinal / 1000);
-    let minutos = Math.floor(segundos / 60);
+    let segundos = Math.floor(tempoFinal /1000);
+    let minutos = Math.floor(segundos /60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
-
-    segundos %=60;
+    
+    segundos %= 60;
     minutos %=60;
     horas %=24;
 
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + "segundos"
+    return dias + " dias " + horas+" horas "+ minutos+" minutos"+segundos +" segundos";
 }
